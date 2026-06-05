58108ffbbd48e3c4fe20b2c092972c21f16ca551
Oppie1 <atindall729@gmail.com>
2026-06-05 18:26:17 -0500

    Add files via upload

---
+++ 1.7CVariables.JLsNts/1.7CVariables.JLsNts/main.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+
+
+
+/*Variables are named storage locations in memory that hold values. Think of them like labeled
+boxes where you can store and retrieve data, similar to how you use variables in algebra*/
+
+
+int main()
+{
+
+	//Declare 3 integer variables: age, currentYear, birthYear.
+	//CODE:
+
+	//Now initialize the integer variables with specific values (years). These values are set directly in the code 
+	//not entered by the user.
+	//CODE:
+
+	//Write an expression that assigns the age equal to the result of the current year minus the birth year.
+	
+	//CODE:
+
+	//When this line executes, the calculation proceeds as follows and is stored in int age:
+	//age = 2026 - 1983, which results in age = 43.
+
+	//Print message to screen that say Kanopi is... years old and use the age variable and data type specifier to do so.
+	//CODE:
+
+}
---
+++ 1.8n9CStringTerminator.JLsNts/1.8n9CStringTerminator.JLsNts/main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+int main() {
+
+	//Declare a char array 'name' with 14 elements and assign it the string "Kanopi Lotus"
+	//The array size must be large enough to hold all characters in sentence or you'll get an error.
+	//CODE:
+
+	//Print a greeting message to the screen using the string stored in 'name'.
+	//CODE:
+
+	//Declare a char array called 'food' with 13 elements and initialize it to "soybean".
+	//CODE:
+
+	//Show to the screen using a sentence is the best food in the world. 
+	//CODE:
+
+		//strcpy() (string copy) from <string.h> overwrites the current contents of a char array.
+		//Remember all strcpy() is a function with parameters.
+		//with a new string value. Here, replace "soybean" with "peanuts". 
+		//CODE:
+
+	//Print the updated value of 'food' to confirm strcpy() replaced.
+	//CODE:
+
+}
