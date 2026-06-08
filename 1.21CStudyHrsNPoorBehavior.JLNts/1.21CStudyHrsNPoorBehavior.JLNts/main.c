f8b3c833f8788876aed9faee4534d955f4e33c86
Oppie1 <atindall729@gmail.com>
2026-06-07 23:02:15 -0500

    Add files via upload

---
+++ 1.21CStudyHrsNPoorBehavior.JLNts/1.21CStudyHrsNPoorBehavior.JLNts/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+	//Declare two uninitialized variable to hold values for hours a student studied and
+	//another for if they got any marks for bad behavior.
+	//CODE:
+
+	//call a printf() function to ask user how many hours they studied. 
+	//CODE:
+
+	//call a scanf_s() function to scan in the information from the user input. &
+	//CODE:
+
+	//Now use the same logic to prompt user and see in information into the
+	//poor behavior variable.
+	//First a printf() call then a scanf_s call.
+	//CODE:
+
+	//CODE:
+
+	//Use an if statement to check if hours studied are more than 10 hours AND
+	//another to see if they got ANY && marks for bad behavior. If they fail either
+	//condition use else statement to tell them to go see counselor.
+	//REMEMBER you will need to use parenthesis for both arguments and the () to keep them together.
+	//CODE:
+
+
+}
