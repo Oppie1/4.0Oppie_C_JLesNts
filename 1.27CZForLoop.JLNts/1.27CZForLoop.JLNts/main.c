3f7646af52b86e7765ac23f79b1fb1495b258116
Oppie1 <atindall729@gmail.com>
2026-06-08 18:36:24 -0500

    Add files via upload

---
+++ 1.27CZForLoop.JLNts/1.27CZForLoop.JLNts/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main() {
+
+	//Declare two uninitialized integer variables named peanuts and soybeans.
+	//CODE:
+
+	//This peanut loop counts from 0 up to 10 incrementing by 2 on each iteration. The loop variable peanuts shadows
+	//the one declared above that was uninitialized. - it is a separate local variable to this specific loop.
+	//Use a for loop to initialize peanut to 0. Have loop run so long as peanut is less than 10, increment by 2 each iteration.
+	//CODE:
+
+		//Show to screen a sentence that says how many peanuts their are during each iteration.
+		//CODE:
+
+	printf("\nAnd now for the second example\n\n");
+
+	//Create a for loop where soybean int value starts at 1. For the 
+	//control condition use soybean <= 10 again but this time just increment by 1.
+	//CODE:
+
+	//Now same as above peanut example print out how many soybeans you have for all 10 iterations.
+		//CODE:
+}
