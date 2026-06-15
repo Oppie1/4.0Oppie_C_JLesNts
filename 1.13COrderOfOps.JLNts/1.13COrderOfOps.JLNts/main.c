a9d2179f4b9307c870a5485d8f74ef17fe41c2a6
Oppie1 <atindall729@gmail.com>
2026-06-14 19:13:19 -0500

    Add files via upload

---
+++ 1.13COrderOfOps.JLNts/1.13COrderOfOps.JLNts/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+//Order of Operations: C follows standard math precedence rules (like PEDMAS).
+
+
+int main()
+{
+
+	//Declare an int variable "a" and set equal to an expression where multiplication happens first even 
+	//though then two values being added together. Will equal 6.
+	//CODE:
+
+	//Call print() to show the value of your int variable to the screen.
+	//CODE:
+
+	//'a' can be reassigned at any point without repeating 'int' data type before it. Can just say 'a' = this.
+	//The data type is only needed when first declared.
+	//Assign a to sn expression put parenthesis around the addition portion while leaving
+	//the multiplication portion as is. Will equal 8. First a assignment stays the same (still 6).
+	//CODE:
+
+	//Call print() to show the value of int variable a.  Notice the difference in both outputs.
+	//CODE:
+
+}
